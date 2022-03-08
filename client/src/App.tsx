0a127d5fc838947177a40ca606db2706a2517432
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2022-03-07 21:34:28 -0600

    feat: Adding Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import TaskForm from "./components/TaskForm/TaskForm";
+import TaskList from "./components/TaskList.tsx/TaskList";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/tasks/new" element={<TaskForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
