450bc48efe268088df7c7a390639292f1cfb8413
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2022-03-07 21:18:08 -0600

    feat: using Cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const taskRoutes = require("./routes/tasks.routes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(taskRoutes);
