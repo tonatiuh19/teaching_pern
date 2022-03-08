84080444201b1aee2f61f1bdda362532cca02818
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2022-03-07 21:58:55 -0600

    feat: adding material ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import TaskForm from "./components/TaskForm/TaskForm";
 import TaskList from "./components/TaskList.tsx/TaskList";
+import { Container } from "@mui/material";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/tasks/new" element={<TaskForm />} />
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,43 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="transparent">
+          <Container>
+            <Toolbar>
+              <Typography sx={{ flexGrow: 1 }} variant="h6">
+                <Link
+                  to="/"
+                  style={{ textDecoration: "none", color: "aliceblue" }}
+                >
+                  Learning PERN
+                </Link>
+              </Typography>
+              <Button
+                variant="contained"
+                onClick={() => navigate("/tasks/new")}
+              >
+                New task
+              </Button>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
+    </div>
+  );
 };
 
 export default NavBar;
